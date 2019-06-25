9b2a4ab089e9d8378b274e431168abf729f9b8fe
Mila <milmil@ukr.net>
2019-06-25 10:56:22 +0300

    add fb, insta, telegram buttons to FooterButtons

--- src/App/Footer/Footer.js
+++ src/App/Footer/Footer.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import './footer.css'
+import FooterButtons from './FooterButtons'
 
 
 const Footer = () => {
 	return (
 		<footer className="footer">
-			Footer
+			<div className="container">
+				<div className="row">
+
+					<div className="col-lg-12">
+						<div className="join">Join us:</div>
+						<FooterButtons/>
+					</div>
+
+					
+				</div>
+			</div>
 		</footer>
 	)
 }
---
+++ src/App/Footer/FooterButtons.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import fb from './fb.png'
+import insta from './insta.png'
+import telegram from './telegram.png'
+
+
+
+const FooterButtons = () => {
+    return (
+        <div className="header-buttons">
+			<div class="fb">
+                <a href="https://www.facebook.com/" target="_blank">
+                    <img src={fb} alt="" />
+                </a>
+            </div>
+
+			<div class="insta">
+                <a href="https://www.instagram.com/" target="_blank">
+                    <img src={insta} alt="" />
+                </a>    
+            </div>
+
+            <div class="telegram">
+                <a href="https://tlgrm.ru/channels/" target="_blank">
+                    <img src={telegram} alt="" />
+                </a>            
+            </div>
+        </div>
+    )
+}
+
+export default FooterButtons
